85a61b788cfacc2c77e70c12c46eb345cd69f23b
DavidSC <david@wbcboxing.com>
2021-04-02 05:30:44 -0600

    desde la version 7 agrega tipado a las func

--- 15-funciones.php
+++ 15-funciones.php
@@ -1,7 +1,22 @@
-<?php include 'includes/header.php';
+<?php
+
+declare(strict_types=1); //asegura un tipado estricto mayor calidad al escribir codigo
+include 'includes/header.php';
 
 
+function sumar(int $numero1 = 0, int  $numero2 = 0)
+{
+    echo $numero1 + $numero2;
+}
 
+sumar(10, 20);
+echo "<br>";
+
+sumar(1, 20);
+echo "<br>";
+
+sumar(100, 30);
+echo "<br>";
 
 
 include 'includes/footer.php';
