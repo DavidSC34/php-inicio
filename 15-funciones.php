76a4d927b9ec67cf668cc6106bb1e6f9bc73bc17
DavidSC <david@wbcboxing.com>
2021-04-02 22:35:31 -0600

    Parametros nombrados desde PHP 8

--- 15-funciones.php
+++ 15-funciones.php
@@ -6,17 +6,13 @@ include 'includes/header.php';
 
 function sumar(int $numero1 = 0, int  $numero2 = 0)
 {
+    echo  $numero1;
     echo $numero1 + $numero2;
 }
 
-sumar(10, 20);
-echo "<br>";
+sumar(numero2: 10, numero1: 20);
+sumar(numero1: 10, numero2: 20);
 
-sumar(1, 20);
-echo "<br>";
-
-sumar(100, 30);
-echo "<br>";
 
 
 include 'includes/footer.php';
