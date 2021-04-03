71e2fd2832cdb1883cd038d857ed1ecc7eed1771
DavidSC <david@wbcboxing.com>
2021-04-02 22:53:37 -0600

    Importancia de require, include, require_once

--- 17-require.php
+++ 17-require.php
@@ -1,7 +1,11 @@
-<?php include 'includes/header.php';
+<?php require 'includes/header.php';
 
+//include sigue ejecutandose
+//requiere es mas critico para seguir con la ejecucion de la app
+//require_once - si el archivo ya fue cargado lo ignora 
+require 'funciones.php';
 
-
+iniciarApp();
 
 
 
---
+++ funciones.php
@@ -0,0 +1,5 @@
+<?php
+function iniciarApp()
+{
+    echo "Inniciar app..";
+}
